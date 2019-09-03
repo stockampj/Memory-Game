f0da8fc9a39766828e00e0e77ff90d263921accd
Jordan King and joel stockamp <jordan@>
2019-09-03 15:10:10 -0700

    add two constructors and a prototype that returns an array of cards randomized

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { pingPong } from './ping-pong';
+import { pingPong } from './memory-game.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/memory-game.js
+++ src/memory-game.js
@@ -13,3 +13,49 @@ export function pingPong(goal) {
   }
   return output;
 }
+
+function MemoryGame(){
+  this.cards = [a, aa, b, bb, c, cc, d, dd, e, ee, f, ff],
+  this.arrangement = []
+}
+
+MemoryGame.prototype.randomizer = function (cards) {
+  var test = memoryGame.cards;
+  var duplicateProtector = [];
+  for (var i=0; duplicateProtector.length<12; i++) {
+    var randInd = Math.floor(Math.random()*12);
+    if (duplicateProtector.indexOf(randInd) === -1) {
+      duplicateProtector.push(randInd);
+    }
+  }
+  for (var j=0; j<12 ; j++) {
+      var newIndex = duplicateProtector[j]
+      memoryGame.arrangement.push(test[newIndex]);
+  }
+}
+
+
+
+function MemoryCard(cardName, cardImageURL, cardID) {
+  this.cardName = cardName,
+  this.cardImageURL = cardImageURL,
+  this.cardID = cardID
+}
+
+var a = new MemoryCard ("turtle", "turtle", 1)
+var aa = new MemoryCard ("turtle", "turtle", 2)
+
+var b = new MemoryCard ("dog", "dog", 3)
+var bb = new MemoryCard ("dog", "dog", 4)
+
+var c = new MemoryCard ("rabbit", "rabbit", 5)
+var cc = new MemoryCard ("rabbit", "rabbit", 6)
+
+var d = new MemoryCard ("hippo", "hippo", 7)
+var dd = new MemoryCard ("hippo", "hippo", 8)
+
+var e = new MemoryCard ("panda", "panda", 9)
+var ee = new MemoryCard ("panda", "panda", 10)
+
+var f = new MemoryCard ("lion", "lion", 11)
+var ff = new MemoryCard ("lion", "lion", 12)
